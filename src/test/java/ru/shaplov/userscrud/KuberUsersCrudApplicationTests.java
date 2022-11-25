8175359b6e30942d6d38a627bb26970d9c420975
shaplovdv <shaplovd@gmail.com>
2022-11-25 17:59:29 +0300

    init commit

---
+++ src/test/java/ru/shaplov/userscrud/KuberUsersCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shaplov.userscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberUsersCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
